65b7e0fab565694b5043b6a0cfc950cfc99d6e00
Telicia <teli203@aol.com>
2025-07-02 23:18:07 -0500

    Create notes for work around with api and browser settings, create a function for twitter to open with quote and author if signed in, if not you can sign in and go from there

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const author = document.getElementById("author");
 const api_url = "https://api.quotable.io/random";
 const newQuoteBtn = document.getElementById("new-quote");
 
+//API call for quotes & fails
 async function getQuote(url) {
  try {
     const response = await fetch(url);
@@ -16,6 +17,11 @@ async function getQuote(url) {
     }
 }
 
-getQuote(api_url);
+getQuote(api_url); //If browser is blocking api go here https://api.quotable.io/random to check for warnings and advance from there
 
 newQuoteBtn.addEventListener("click", () => getQuote(api_url));
+
+// Open twitter with quote & author
+function tweet() {
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "  -- by " + author.innerHTML, "Tweet Window");
+}
